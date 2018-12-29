5960c6d9434cd916656871642cadcea7059d38e8
mohit <mohit@mohit-PC>
2018-12-29 14:37:01 +0530

    commit

--- src/Test_NG_Package3/Test_NG_Class32.java
+++ src/Test_NG_Package3/Test_NG_Class32.java
@@ -2,4 +2,10 @@ package Test_NG_Package3;
 
 public class Test_NG_Class32 {
 
+	int a = 1;
+	int b = 2;
+	public static void main (String[] are){
+		
+		System.out.println("kjadbcjkabcjkasb");
+	}
 }
