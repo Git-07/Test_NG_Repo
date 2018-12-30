f4bf0cb53ca74d28dcb9a3e9b6784bff0c56f5b0
Git-07 <43235173+Git-07@users.noreply.github.com>
2018-12-30 08:57:58 +0530

    Update run.bat
    cc

--- lib/run.bat
+++ lib/run.bat
@@ -1,5 +1,3 @@
-git clone https://github.com/Git-07/Test_NG_Repo.git Test_Repo
-cd Test_Repo
 set projectPath=C:\Users\mohit\.jenkins\workspace\Run_Test_NG_Git\Test_Repo
 set classpath=C:\Users\mohit\workspace\Test_NG\bin;%projectPath%\lib\*;
 java org.testng.TestNG testng.xml
